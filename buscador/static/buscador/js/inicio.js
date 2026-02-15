bee475bd928463d30a741c31027d42d5438ef30a
Josefina <jhalcartegaray@gmail.com>
2026-02-15 08:54:57 -0300

    Kimoon V1 - Página estilizada

--- buscador/static/buscador/js/inicio.js
+++ buscador/static/buscador/js/inicio.js
@@ -8,25 +8,13 @@ document.addEventListener("DOMContentLoaded", function()
         const params = new URLSearchParams(window.location.search);
         params.set("q", query);
 
-        params.set("q", query);
-
         fetch(`?${params.toString()}`)
             .then(response => response.text())
             .then(html => {
                 const newDoc = new DOMParser().parseFromString(html, "text/html")
-                const ul = newDoc.querySelector("#actividades ul");
-                const actividades = document.querySelector("#actividades");
-
-                if(ul)
-                {
-                    actividades.innerHTML = `<ul>${ul.innerHTML}</ul>`;
-                }
-                else
-                {
-                    actividades.innerHTML = `<ul>${ul.innerHTML}</ul>`;
-                }
+                const contenedor = newDoc.querySelector("#contenedor-actividades");
+                document.querySelector("#contenedor-actividades").innerHTML = contenedor.innerHTML;
             })
-            .catch(err => console.error(err))
     })
 })
 
